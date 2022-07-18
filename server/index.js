745522b77c98ff1a407a38a91bfc898204760d5d
Hadassatech <80362410+Hadassatech@users.noreply.github.com>
2022-07-18 23:35:40 +0300

    cache

--- server/index.js
+++ server/index.js
@@ -5,14 +5,18 @@ const axios = require("axios");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
+const apicache = require("apicache");
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+let cache = apicache.middleware
+app.use(cache('5 minutes'))
 
 app.get("/getdata", function (req, res) {
   const fetchData = async () => {
     try {
-      const data = await axios("https://picsum.photos/v2/list");
+      const data = await axios("https://www.omdbapi.com/?s=requiem&apikey=ae16d87c");
       res.send(data.data);
     } catch (e) {
       console.log("err", e);
